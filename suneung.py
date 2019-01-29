7ac361e341815c180e823c9b3a312d4452d16736
wilee <wilee1224@gmail.com>
2019-01-29 23:27:05 +0900

    pdf오픈 및 최대화까지 구현

--- suneung.py
+++ suneung.py
@@ -2,14 +2,29 @@ import subprocess
 import os
 import time
 import keyboard
-import pyHook
+from pynput.mouse import Listener
+
+# mouse  = Controller
 
 def openPdf():
-    # subprocess.Popen("수학(가형)_홀.pdf", shell=True)
-    # open("수학(가형)_홀.pdf")
+    f = open("수학(가형)_홀.pdf")
     os.startfile("수학(가형)_홀.pdf")
     time.sleep(3)
     keyboard.press_and_release('ctrl+l')
+    with Listener(on_click=on_click) as listener:
+        listener.join()
+    f.close()
+
+def on_click(x, y, button, pressed):
+    # print('{0} at {1}'.format('Pressed' if pressed else 'Released', (x, y)))
+    print('{1}'.format('pressed' if pressed else 'Released', (x, y)))
+    if not pressed:
+
+        # Stop listener
+        return False
+
+
+# Collect events until released
 
 
 if __name__ == '__main__':
